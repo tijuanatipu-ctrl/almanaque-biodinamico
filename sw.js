02f3c52f561bf4ffeee56cb3d5b4ea196f18e7aa
tijuanatipu <tijuanatipu@gmail.com>
2026-05-25 10:48:35 -0300

    Almanaque Biodinamico PWA

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'almanaque-v1';
+
+const ASSETS = [
+  './almanaque_biodinamico.html',
+  './manifest.json',
+  './icon.svg',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Cache-first: sirve desde cache, actualiza en background si hay red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type !== 'opaque') {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      }).catch(() => {});
+      return cached || network;
+    })
+  );
+});
